f2bd2db352c87bcc63f7257d3f86c44dec2288bd
Juergen Ryannel <juergen@ryannel.org>
2020-09-20 16:37:31 +0200

    some updates

--- integration/src/index.ts
+++ integration/src/index.ts
@@ -2,4 +2,11 @@ import { CommandLine } from "@mytool/cli";
 import { Server } from "@mytool/Server";
 import { Core } from "@mytool/core";
 
-console.log();
+const cli = new CommandLine();
+cli.run();
+
+const server = new Server();
+server.run();
+
+const core = new Core();
+core.visit("foo");
